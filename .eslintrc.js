4990bf415b02ad6bc3e5d6b88118e18d1c210fd8
age <a.g.estaun@gmail.com>
2023-06-27 19:51:28 +0200

    Updating eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,37 +4,40 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:@typescript-eslint/recommended',
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script',
       },
     },
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    }
   },
-  plugins: [
-    'react',
-    'react-native',
-  ],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
   rules: {
     'no-console': 'off',
     'no-var': 'error',
     'no-use-before-define': 'off',
+    'react-native/no-color-literals': 'error',
     'react-native/no-unused-styles': 'error',
     'react-native/no-inline-styles': 'error',
     'react/style-prop-object': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
     'react/jsx-filename-extension': ['warn', { extensions: ['.tsx', '.ts'] }],
   },
