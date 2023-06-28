ef1ee7aadaf7a3f648869db18378ca4610f0b37d
age <a.g.estaun@gmail.com>
2023-06-28 19:34:07 +0200

    Defining the screen/route names and adding workaround to allow React Navigation to check the types in compilation time. It also allows to define params to check the types in compilation time.

---
+++ src/navigation/NavRouteList.ts
@@ -0,0 +1,19 @@
+// Any new screen/route and param must be defined here for type checking.
+export type NavRouteList = {
+  Home: undefined;
+  Game: undefined;
+  GameOver: undefined;
+  Scores: undefined;
+  Settings: undefined;
+};
+
+// This registers which makes navigation fully type-safe. I need to skip some eslint rules here.
+// https://reactnavigation.org/docs/typescript#specifying-default-types-for-usenavigation-link-ref-etc
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    // This is needed to overwrite the default type
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface RootParamList extends NavRouteList {}
+  }
+}
