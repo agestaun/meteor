e63442e110d84b5bc522819c8e5f1dcc01774701
age <a.g.estaun@gmail.com>
2023-06-28 18:01:18 +0200

    Creating Base.ts to contain the base styles for the app. Spacing, radius and font size for now.

---
+++ styles/Base.ts
@@ -0,0 +1,33 @@
+export const spacing = {
+  xxs: 2,
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+};
+
+export const radius = {
+  xxs: 2,
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 20,
+  xxl: 24,
+};
+
+export const fonts = {
+  size: {
+    xxs: 10,
+    xs: 12,
+    sm: 13,
+    md: 14,
+    lg: 18,
+    xl: 24,
+    xxl: 28,
+    "2xl": 32,
+    "3xl": 36,
+  },
+};
