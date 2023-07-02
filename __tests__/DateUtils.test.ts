659a584bcdbb1ebeaf32c8b59f5077d6fdd8defd
age <a.g.estaun@gmail.com>
2023-07-03 00:28:19 +0200

    Updating format for the time and the corresponding  tests.

--- __tests__/DateUtils.test.ts
+++ __tests__/DateUtils.test.ts
@@ -1,45 +1,59 @@
-import { timestampToSecondsAndMillis } from "../src/utils/DateUtils";
+import { formatTimestampToString } from "../src/utils/DateUtils";
 
 describe("Date Utils Tests", () => {
   it("should return 2 seconds and 00 milliseconds", function () {
     const milliseconds = 2000;
-    const expected = "2,00";
-    const result = timestampToSecondsAndMillis(milliseconds);
+    const expected = "2,00 seconds";
+    const result = formatTimestampToString(milliseconds);
     expect(result).toBe(expected);
   });
 
   it("should return 5 seconds and 55 milliseconds", function () {
     const milliseconds = 5557;
-    const expected = "5,55";
-    const result = timestampToSecondsAndMillis(milliseconds);
+    const expected = "5,55 seconds";
+    const result = formatTimestampToString(milliseconds);
     expect(result).toBe(expected);
   });
 
   it("should return 5 seconds and 05 milliseconds", function () {
     const milliseconds = 5057;
-    const expected = "5,05";
-    const result = timestampToSecondsAndMillis(milliseconds);
+    const expected = "5,05 seconds";
+    const result = formatTimestampToString(milliseconds);
     expect(result).toBe(expected);
   });
 
   it("should return 3 seconds and 02 milliseconds", function () {
     const milliseconds = 3002;
-    const expected = "3,00";
-    const result = timestampToSecondsAndMillis(milliseconds);
+    const expected = "3,00 seconds";
+    const result = formatTimestampToString(milliseconds);
     expect(result).toBe(expected);
   });
 
   it("should return 0 seconds and 00 milliseconds", function () {
     const milliseconds = 0;
-    const expected = "0,00";
-    const result = timestampToSecondsAndMillis(milliseconds);
+    const expected = "0,00 seconds";
+    const result = formatTimestampToString(milliseconds);
     expect(result).toBe(expected);
   });
 
   it("should return 10 seconds and 06 milliseconds", function () {
     const milliseconds = 10608;
-    const expected = "10,60";
-    const result = timestampToSecondsAndMillis(milliseconds);
+    const expected = "10,60 seconds";
+    const result = formatTimestampToString(milliseconds);
+    expect(result).toBe(expected);
+  });
+
+  it("should return 2 minutes 0,60 seconds", function () {
+    const milliseconds = 120608;
+    const expected = "2 minutes 0,60 seconds";
+    const result = formatTimestampToString(milliseconds);
+    expect(result).toBe(expected);
+  });
+
+  it("should return 10 minutes 14,12 seconds", function () {
+    const milliseconds = 134120;
+    const expected = "2 minutes 14,12 seconds";
+    const result = formatTimestampToString(milliseconds);
     expect(result).toBe(expected);
   });
 });
